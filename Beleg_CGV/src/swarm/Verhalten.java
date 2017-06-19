776f6fdf34a377c98147de98876fbc41a128ba23
Schnitzel128 <schnitzel128@gmail.com>
2017-06-19 15:17:08 +0200

    update

---
+++ Beleg_CGV/src/swarm/Verhalten.java
@@ -0,0 +1,5 @@
+package swarm;
+
+public interface Verhalten {
+   public void update() throws Exception;
+}
---
+++ Beleg_CGV/src/swarm/VerhaltenZufälligerFlummi.java
@@ -0,0 +1,23 @@
+package swarm;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import math.Vektor2D;
+
+public class VerhaltenZufälligerFlummi implements Verhalten {
+   private Flummi flummi;
+   private Random rand = ThreadLocalRandom.current();
+   
+   public VerhaltenZufälligerFlummi(Flummi flummi) {
+      this.flummi = flummi;
+   }
+   
+   @Override
+   public void update() throws Exception {
+      flummi.position.add(new Vektor2D(0, rand.nextFloat()*2 - 1));
+
+      if (flummi.position.y>480 || flummi.position.y<0)
+         flummi.velocity.y *= -1;
+   }
+}
